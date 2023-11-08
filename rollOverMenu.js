50e03916f98bdba9f70f12157bd1d88d76858aa3
Fredrik Jakob Talleraas Emblem <fjemblem@stud.ntnu.no>
2023-11-08 16:33:14 +0100

    Dynamic pages is working

--- rollOverMenu.js
+++ rollOverMenu.js
@@ -1,15 +1,25 @@
-// Get all the menu items
-const menuItems = document.querySelectorAll('.menu li');
-
-// Add a hover event listener to each menu item
-menuItems.forEach((menuItem) => {
-    menuItem.addEventListener('mouseenter', () => {
-        // Change the background color on hover
-        menuItem.style.backgroundColor = '#f0f0f0';
-    });
-
-    menuItem.addEventListener('mouseleave', () => {
-        // Restore the background color on mouse leave
-        menuItem.style.backgroundColor = 'transparent';
-    });
+document.getElementById('home').addEventListener('click', function() {
+    // Redirect to the home page
+    window.location.href = "homepage.html";
 });
+
+document.getElementById('about').addEventListener('click', function() {
+    // Redirect to the home page
+    window.location.href = "about.html";
+});
+
+document.getElementById('menu').addEventListener('click', function() {
+    // Redirect to the home page
+    window.location.href = "menu.html";
+});
+
+document.getElementById('contact').addEventListener('click', function() {
+    // Redirect to the home page
+    window.location.href = "contactUs.html";
+});
+
+document.getElementById('opening').addEventListener('click', function() {
+    // Redirect to the home page
+    window.location.href = "openingHours.html";
+});
+
